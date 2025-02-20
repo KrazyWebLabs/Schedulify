acbb0fbb2c834e567dad0c039c6951e78cff65d3
CodingGaby <macma.cube1@gmail.com>
2025-02-20 08:29:10 -0600

    fix(app): Improved Buttons
    Buttons now look good :D

--- src/components/buttons/BackBtn.tsx
+++ src/components/buttons/BackBtn.tsx
@@ -7,9 +7,9 @@ export default function BackBtn() {
   return (
     <a
       href={previousPath}
-      className="inline-block px-4 py-2 mr-4 text-white duration-150 font-medium bg-boston-blue-400 rounded-lg hover:bg-boston-blue-500 active:bg-boston-blue-700 md:text-sm capitalize"
+      className="flex justify-center px-4 min-w-full py-2 text-white duration-150 font-bold bg-boston-blue-400 rounded-lg hover:bg-boston-blue-500 active:bg-boston-blue-700 md:text-sm capitalize"
     >
-      <p className="text-lg">back</p>
+      back
     </a>
   );
 }
--- src/components/buttons/SubmitBtn.tsx
+++ src/components/buttons/SubmitBtn.tsx
@@ -2,7 +2,7 @@ export default function SubmitBtn({ children }: { children: React.ReactNode }) {
   return (
     <button
       type="submit"
-      className="inline-block px-4 min-w-full py-2 mx-2 text-white duration-150 font-medium bg-boston-blue-600 rounded-lg hover:bg-boston-blue-500 active:bg-boston-blue-700 md:text-sm capitalize"
+      className="inline-block px-4 min-w-full py-2 text-white duration-150 bg-boston-blue-600 rounded-lg hover:bg-boston-blue-500 active:bg-boston-blue-700 md:text-sm capitalize font-bold"
     >
       {children}
     </button>
